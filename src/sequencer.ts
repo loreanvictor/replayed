197cbd025e06bae2a89b96d1cb2db880ad34b484
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-11-07 15:48:02 +0100

    document sequencer for future ref

--- src/sequencer.ts
+++ src/sequencer.ts
@@ -1,11 +1,51 @@
 export interface SequencedEvent { seq: number }
 
-
+/**
+ *
+ * Ensures events are replayed according to given
+ * sequence. This helps ensure that replaying functions will yield
+ * stable results based on the order of output events. Take the following
+ * example:
+ *
+ * ```ts
+ * const res = await Promise.race([stepA(), stepB()])
+ * ```
+ *
+ * If `stepB` completes first, then initially `res` will be set to its result.
+ * However, if until some future replay, `stepA` has also completed, the result of this
+ * expression, without sequencing, will be `stepA`'s result, meaning replays will
+ * behave differently.
+ *
+ * To fix this issue, we use the sequencer to ensure that `stepA()` and `stepB()`
+ * will complete in the same order as in the event log. Each step/hook that is already completed,
+ * waits for the sequencer of each run before returning its value or throwing its failure error,
+ * and the sequencer allows these steps/hooks to proceed based on the order determined
+ * by the given sequence.
+ *
+ * @param events the sequence of events that should be enforced
+ * @returns an object with a `turn()` method. Assuming the enforced sequence is [..., i, j, ...],
+ *          calling `turn(j)` yields a Promise that is only resolved after `turn(i)`
+ *          has been called and its Promise resolved, i.e. after all prior events have been handled.
+ */
 export const sequencer = (events: SequencedEvent[]) => {
   const iterator = events[Symbol.iterator]()
+  //
+  // we only resolve promises for calls
+  // to the sequence pointed to by the cursor, and anyone else will just
+  // be enqueued and will have to wait.
+  //
   let cursor = iterator.next()
+
+  //
+  // holds a queue for all the waiters. each sequence number
+  // can have multiple waiters, and we won't proceed to the next
+  // sequence until all waiters have been resolved.
+  //
   const queue = new Map<number, (() => void)[]>()
 
+  //
+  // puts a waiter in the queue for given sequence.
+  //
   const enqueue = (seq: number, fn: () => void) => {
     const fns = queue.get(seq)
     if (fns) {
@@ -15,6 +55,10 @@ export const sequencer = (events: SequencedEvent[]) => {
     }
   }
 
+  //
+  // removes a specific waiter from the queue of given sequence.
+  // if no other waiters remain, will also remove the queue.
+  //
   const dequeue = (seq: number, fn: () => void) => {
     if (queue.has(seq)) {
       const fns = queue.get(seq)!
@@ -28,6 +72,18 @@ export const sequencer = (events: SequencedEvent[]) => {
     }
   }
 
+  //
+  // checks whether we should proceed to the next sequence.
+  // the caller must have current sequence, which is the input argument
+  // that is checked against the current sequence.
+  //
+  // if there are other waiters for the current sequence, won't proceed
+  // to the next one, and calls the next waiter. each waiter in turn then
+  // calls this function when they are resolved.
+  //
+  // if no further waiters remain for current sequence, we go to the next
+  // sequence, and if there are any waiters, will call the first one.
+  //
   const next = (seq: number) => {
     if (cursor.done) { return }
     if (cursor.value.seq !== seq) {
@@ -51,12 +107,25 @@ export const sequencer = (events: SequencedEvent[]) => {
 
   return {
     turn: (seq: number) => new Promise<void>((resolve, reject) => {
+      //
+      // checks whether the current sequence is the one
+      // that we are waiting for with this `turn()` call.
+      //
       const check = () => {
         if (cursor.done) {
+          //
+          // actually no more events in the sequence,
+          // there must have been an error that needs resolving.
+          //
           reject()
 
           return false
         } else if (seq === cursor.value.seq) {
+          //
+          // ok we are at the correct sequence, so we can remove ourselves
+          // from the queue, and call on the next handler or next sequence
+          // if there are no more handlers for current seq.
+          //
           dequeue(seq, check)
           next(seq)
           resolve()
@@ -64,6 +133,10 @@ export const sequencer = (events: SequencedEvent[]) => {
           return false
         }
 
+        //
+        // eh, we should wait apparently, so
+        // lets just enqueue ourselves and wait.
+        //
         return true
       }
 
